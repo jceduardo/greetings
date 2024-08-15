390020fc0d3565882a387287dca410c8c33a4194
Juan Carlos Eduardo Hidalgo <juan.eduardo@globant.com>
2024-08-15 12:03:15 -0500

    greetings-v1

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-"# greetings" 
+# Saludos en Go
+
+Este paquete proporciona una forma simple de obtener saludos personalizados en Go.
+
+## Instalación
+Ejecuta el siguiente comando para instalar el paquete:
+
+```bash
+go get -u github.com/jceduardo/greetings
+```
+
+## Uso
+Aquí tienes un ejemplo de cómo utilizar el paquete en tu código:
+
+```go
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jceduardo/greetings"
+)
+
+func main() {
+	log.SetPrefix("greetings: ")
+	//log.SetFlags(0)
+
+	names := []string{"Juan", "Vivi", "Alex"}
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(messages)
+}
+```
+
--- greetings.go
+++ greetings.go
@@ -1,8 +1,40 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
-func Hello(name string) string {
-	message := fmt.Sprintf("Hola, %v. Bienvenido!", name)
-	return message
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return name, errors.New("Nombre vacio")
+	}
+
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hola, %v, Bienvenido!",
+		"Que bueno verte, %v!",
+		"Saludo, %v encantado de conocerte!",
+	}
+	return formats[rand.Intn(len(formats))]
 }
